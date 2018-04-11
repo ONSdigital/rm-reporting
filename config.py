c4168c0c8fe8e8e6862e3c734a8265cb465361f6
lewis <lewis.babbage@ons.gsi.gov.uk>
2018-04-11 13:49:01 +0100

    Fix pep8 issue

--- config.py
+++ config.py
@@ -19,7 +19,8 @@ class Config(object):
         DATABASE_URI = cf.db.credentials['uri']
     else:
         DATABASE_URI = os.getenv('DATABASE_URI',
-            "postgres://uojlz5705c2z6js3:8zhscuv4icdgs3cuurs5e0zo1@localhost:5430/db4mardgluago2k7w")
+                                 "postgres://uojlz5705c2z6js3:8zhscuv4icdgs3cuurs5e0zo1@localhost:5430/"
+                                 "db4mardgluago2k7w")
 
 
 class DevelopmentConfig(Config):
