b6e49b84b8c7a7c0f014e03e7475e1b5443acb21
AdamHawtin <adam.hawtin@ons.gov.uk>
2018-05-15 16:24:37 +0100

    Reformat string query and first pass error handling

--- rm_reporting/resources/responses_dashboard.py
+++ rm_reporting/resources/responses_dashboard.py
@@ -4,6 +4,7 @@ import logging
 from flask import json, Response
 from flask_restplus import Resource
 from sqlalchemy import text
+from sqlalchemy.exc import DataError
 from structlog import wrap_logger
 
 from rm_reporting import app, response_dashboard_api
@@ -13,55 +14,71 @@ logger = wrap_logger(logging.getLogger(__name__))
 
 @response_dashboard_api.route('/<collection_exercise_id>')
 class ResponseDashboard(Resource):
-
     @staticmethod
     def get(collection_exercise_id):
 
         engine = app.db.engine
 
-        collex_status = "SELECT COUNT(DISTINCT(sample_unit_ref)) \"Sample Size\", " \
-                        "SUM(enrolled) \"Total Enrolled\", SUM(downloaded) \"Total Downloaded\", " \
-                        "SUM(uploaded) \"Total Uploaded\" " \
-                        "FROM " \
-                        "(SELECT " \
-                        "events.sampleunitref sample_unit_ref, " \
-                        "events.sampleunittype, " \
-                        "events.caseref case_ref, " \
-                        "events.respondent_enrolled enrolled, " \
-                        "events.collection_instrument_downloaded_ind downloaded, " \
-                        "events.successful_response_upload_ind uploaded " \
-                        "FROM " \
-                        "(SELECT cg.sampleunitref, " \
-                        "c.sampleunittype, c.caseref, " \
-                        "SUM(CASE WHEN ce.categoryFK = 'RESPONDENT_ENROLED' " \
-                        "THEN 1 ELSE  0 END) respondent_enrolled, " \
-                        "MAX(CASE WHEN ce.categoryFK = 'COLLECTION_INSTRUMENT_DOWNLOADED' " \
-                        "THEN 1 ELSE  0 END) collection_instrument_downloaded_ind, " \
-                        "MAX(CASE WHEN ce.categoryFK = 'SUCCESSFUL_RESPONSE_UPLOAD' " \
-                        "THEN 1 ELSE  0 END) successful_response_upload_ind " \
-                        "FROM casesvc.caseevent ce " \
-                        "RIGHT OUTER JOIN casesvc.case c  ON c.casePK = ce.caseFK " \
-                        "INNER JOIN casesvc.casegroup cg  ON c.casegroupFK = cg.casegroupPK " \
-                        "GROUP BY cg.sampleunitref, c.sampleunittype, c.casePK) events) as data_records " \
-                        "WHERE case_ref IN " \
-                        "(SELECT t.caseref FROM casesvc.\"case\" t " \
-                        "WHERE t.casegroupid IN " \
-                        "(SELECT t.id \"Group ID\" FROM casesvc.casegroup t " \
-                        "WHERE t.collectionexerciseid = :collection_exercise_id))"
+        report_query = text(
+            'SELECT '
+            'COUNT(DISTINCT(sample_unit_ref)) "Sample Size", '
+            'SUM(enrolled) "Total Enrolled", '
+            'SUM(downloaded) "Total Downloaded", '
+            'SUM(uploaded) "Total Uploaded" '
+            'FROM '
+            '(SELECT '
+            'events.sampleunitref sample_unit_ref, '
+            'events.sampleunittype, '
+            'events.caseref case_ref, '
+            'events.respondent_enrolled enrolled, '
+            'events.collection_instrument_downloaded_ind downloaded, '
+            'events.successful_response_upload_ind uploaded '
+            'FROM '
+            '(SELECT cg.sampleunitref, '
+            'c.sampleunittype, c.caseref, '
+            'SUM(CASE WHEN ce.categoryFK = \'RESPONDENT_ENROLED\' '
+            'THEN 1 ELSE  0 END) respondent_enrolled, '
+            'MAX(CASE WHEN ce.categoryFK = \'COLLECTION_INSTRUMENT_DOWNLOADED\' '
+            'THEN 1 ELSE  0 END) collection_instrument_downloaded_ind, '
+            'MAX(CASE WHEN ce.categoryFK = \'SUCCESSFUL_RESPONSE_UPLOAD\' '
+            'THEN 1 ELSE  0 END) successful_response_upload_ind '
+            'FROM casesvc.caseevent ce '
+            'RIGHT OUTER JOIN casesvc.case c ON c.casePK = ce.caseFK '
+            'INNER JOIN casesvc.casegroup cg ON c.casegroupFK = cg.casegroupPK '
+            'GROUP BY cg.sampleunitref, c.sampleunittype, c.casePK) events) as data_records '
+            'WHERE case_ref IN '
+            '(SELECT t.caseref FROM casesvc."case" t '
+            'WHERE t.casegroupid IN '
+            '(SELECT t.id "Group ID" FROM casesvc.casegroup t '
+            'WHERE t.collectionexerciseid = :collection_exercise_id))')
 
-        collex_details = engine.execute(text(collex_status), collection_exercise_id=collection_exercise_id).first()
+        try:
+            report_details = engine.execute(
+                report_query,
+                collection_exercise_id=collection_exercise_id).first()
+        except DataError as err:
+            logger.debug("Invalid parameter", invalid_param=err.params)
+            return Response(status=400)
 
-        collex_dict = {
-            'sampleSize': collex_details['Sample Size'],
-            'accountsEnrolled': int(collex_details['Total Enrolled'] or 0),
-            'downloads': collex_details['Total Downloaded'],
-            'uploads': collex_details['Total Uploaded']
+        nulls_returned = any(column is None for column in report_details)
+        if nulls_returned:
+            logger.debug(
+                "Invalid collection exercise ID",
+                collection_exercise_id=collection_exercise_id)
+            return Response(status=400)
+
+        response_content = {
+            'metadata': {
+                'timeUpdated': datetime.now().timestamp(),
+                'collectionExerciseId': collection_exercise_id
+            },
+            'report': {
+                'sampleSize': report_details['Sample Size'],
+                'accountsEnrolled': int(report_details['Total Enrolled']),
+                'downloads': report_details['Total Downloaded'],
+                'uploads': report_details['Total Uploaded']
+            }
         }
 
-        response = {
-            'metadata': {'timeUpdated': datetime.now().timestamp(),
-                         'collectionExerciseId': collection_exercise_id},
-            'report': collex_dict
-        }
-
-        return Response(json.dumps(response), content_type='application/json')
+        return Response(
+            json.dumps(response_content), content_type='application/json')
