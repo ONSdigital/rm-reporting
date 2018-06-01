fe5ee0380115f8fe4b3576b3b8ac66754db8dfb3
AdamHawtin <adam.hawtin@ons.gov.uk>
2018-06-01 09:09:07 +0100

    Update tests for previous change

--- test/resources/test_responses_dashboard.py
+++ test/resources/test_responses_dashboard.py
@@ -20,7 +20,8 @@ class TestResponseDashboard(TestCase):
         }
 
         mock_engine.execute.return_value.first.return_value = valid_response
-        response = self.test_client.get('/reporting-api/v1/response-dashboard/33db9feb-bed0-46e8-bcb1-3a0373224cd3')
+        response = self.test_client.get(
+            '/reporting-api/v1/response-dashboard/collection_exercise/33db9feb-bed0-46e8-bcb1-3a0373224cd3')
         response_dict = json.loads(response.data)
 
         self.assertEqual(100, response_dict['report']['sampleSize'])
@@ -31,14 +32,16 @@ class TestResponseDashboard(TestCase):
     @mock.patch('rm_reporting.app.db.engine')
     def test_dashboard_report_invalid_id(self, mock_engine):
         mock_engine.execute.return_value.first.return_value = (0, 0, 0, None)
-        response = self.test_client.get('/reporting-api/v1/response-dashboard/00000000-0000-0000-0000-000000000000')
+        response = self.test_client.get(
+            '/reporting-api/v1/response-dashboard/collection_exercise/00000000-0000-0000-0000-000000000000')
         error_response = json.loads(response.data)['message']
 
         self.assertEqual(response.status_code, 400)
         self.assertEqual(error_response, 'Invalid collection exercise ID')
 
     def test_dashboard_report_malformed_id(self):
-        response = self.test_client.get('/reporting-api/v1/response-dashboard/not-a-valid-uuid-format')
+        response = self.test_client.get(
+            '/reporting-api/v1/response-dashboard/collection_exercise/not-a-valid-uuid-format')
         error_response = json.loads(response.data)['message']
 
         self.assertEqual(response.status_code, 400)
