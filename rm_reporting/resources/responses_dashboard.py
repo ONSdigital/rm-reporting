2e59c8ec2bd337058d725b0a1186897299768110
Adam <35268982+AdamHawtin@users.noreply.github.com>
2018-11-22 13:40:17 +0000

    Responses Dashboard Report Fixes (#15)
    - Refactor two dashboard figure queries into single query using WITH
    - Filter by survey ID in the party svc tables for an accurate count per collex
    - Add survey ID to endpoint url
    - Remove collection instrument type conditions and url param, no longer required
    - Filter by sampleunitref to exclude dummy sampleunitrefs from count
    - Update tests

--- rm_reporting/resources/responses_dashboard.py
+++ rm_reporting/resources/responses_dashboard.py
@@ -1,7 +1,7 @@
-from datetime import datetime
 import logging
+from datetime import datetime
 
-from flask import json, Response
+from flask import Response, json
 from flask_restplus import Resource, abort
 from sqlalchemy import text
 from structlog import wrap_logger
@@ -13,101 +13,82 @@ from rm_reporting.exceptions import NoDataException
 logger = wrap_logger(logging.getLogger(__name__))
 
 
-def get_case_report_figures(collection_exercise_id, engine):
-    case_query = text('SELECT COUNT(id) AS "Sample Size", '
-                      'COUNT(CASE '
-                      'WHEN status = \'NOTSTARTED\' THEN 1 '
-                      'ELSE NULL '
-                      'END) AS "Not Started", '
-                      'COUNT(CASE '
-                      'WHEN status = \'INPROGRESS\' THEN 1 '
-                      'ELSE NULL '
-                      'END) AS "In Progress", '
-                      'COUNT(CASE '
-                      'WHEN status = \'COMPLETE\' THEN 1 '
-                      'ELSE NULL '
-                      'END) AS "Complete" '
-                      'FROM casesvc.casegroup '
-                      'WHERE collectionexerciseid = :collection_exercise_id')
+def get_report_figures(survey_id, collection_exercise_id, engine):
+    case_query = text('WITH cases AS'
+                      '(SELECT cg.status, cg.sampleunitref, c.sampleunit_id '
+                      'FROM casesvc.casegroup cg '
+                      'INNER JOIN casesvc."case" c ON cg.casegrouppk = c.casegroupfk '
+                      'WHERE cg.collectionexerciseid = :collection_exercise_id '
+                      'AND cg.sampleunitref NOT LIKE \'1%\'), '
+                      'survey_enrolments AS '
+                      '(SELECT e.business_id, e.status, ba.attributes '
+                      'FROM partysvc.enrolment e '
+                      'INNER JOIN partysvc.business_attributes ba ON e.business_id = ba.business_id '
+                      'WHERE survey_id = :survey_id '
+                      'AND collection_exercise = :collection_exercise_id) '
+                      'SELECT COUNT(*) '
+                      'AS "Sample Size", '
+                      'COUNT(CASE WHEN cases.status = \'NOTSTARTED\' THEN 1 ELSE NULL END) '
+                      'AS "Not Started", '
+                      'COUNT(CASE WHEN cases.status = \'INPROGRESS\' THEN 1 ELSE NULL END) '
+                      'AS "In Progress", '
+                      'COUNT(CASE WHEN cases.status = \'COMPLETE\' THEN 1 ELSE NULL END) '
+                      'AS "Complete", '
+                      'COUNT(CASE WHEN survey_enrolments.status = \'ENABLED\' THEN 1 ELSE NULL END) '
+                      'AS "Total Enrolled", '
+                      'COUNT(CASE WHEN survey_enrolments.status = \'PENDING\' THEN 1 ELSE NULL END) '
+                      'AS "Total Pending" '
+                      'FROM cases '
+                      'LEFT JOIN survey_enrolments '
+                      'ON survey_enrolments.attributes ->> \'sampleUnitId\' = cases.sampleunit_id::text')
 
-    return engine.execute(case_query, collection_exercise_id=collection_exercise_id).first()
+    report_figures = engine.execute(case_query,
+                                    survey_id=survey_id,
+                                    collection_exercise_id=collection_exercise_id).first()
 
-
-def get_party_report(collection_exercise_id, engine):
-    party_query = text('SELECT COUNT(CASE enrolment.status WHEN \'ENABLED\' THEN 1 ELSE NULL END) AS "Total Enrolled", '
-                       'COUNT(CASE enrolment.status WHEN \'PENDING\' THEN 1 ELSE NULL END) AS "Total Pending" '
-                       'FROM partysvc.enrolment enrolment '
-                       'INNER JOIN partysvc.business_attributes business_attributes '
-                       'ON business_attributes.business_id = enrolment.business_id '
-                       'WHERE business_attributes.collection_exercise = :collection_exercise_id')
-
-    return engine.execute(party_query, collection_exercise_id=collection_exercise_id).first()
-
-
-def get_report_figures(collection_exercise_id, engine):
-    case_report_figures = get_case_report_figures(collection_exercise_id, engine)
-    party_report_figures = get_party_report(collection_exercise_id, engine)
-
-    if any(column is None for column in list(case_report_figures.values()) + list(party_report_figures.values())):
+    if any(column is None for column in report_figures.values()):
         raise NoDataException
 
-    return case_report_figures, party_report_figures
+    return report_figures
 
 
-def get_seft_report(collection_exercise_id, engine):
-    case_report_figures, party_report_figures = get_report_figures(collection_exercise_id, engine)
-
-    total_downloaded = case_report_figures['In Progress'] + case_report_figures['Complete']
+def get_report(survey_id, collection_exercise_id, engine):
+    report_figures = get_report_figures(survey_id, collection_exercise_id, engine)
 
     return {
-        'sampleSize': case_report_figures['Sample Size'],
-        'accountsPending': party_report_figures['Total Pending'],
-        'accountsEnrolled': party_report_figures['Total Enrolled'],
-        'downloads': total_downloaded,
-        'uploads': case_report_figures['Complete']
+        'inProgress': report_figures['In Progress'],
+        'accountsPending': report_figures['Total Pending'],
+        'accountsEnrolled': report_figures['Total Enrolled'],
+        'notStarted': report_figures['Not Started'],
+        'completed': report_figures['Complete'],
+        'sampleSize': report_figures['Sample Size']
     }
 
 
-def get_eq_report(collection_exercise_id, engine):
-    case_report_figures, party_report_figures = get_report_figures(collection_exercise_id, engine)
-
-    return {
-        'inProgress': case_report_figures['In Progress'],
-        'accountsPending': party_report_figures['Total Pending'],
-        'accountsEnrolled': party_report_figures['Total Enrolled'],
-        'notStarted': case_report_figures['Not Started'],
-        'completed': case_report_figures['Complete'],
-        'sampleSize': case_report_figures['Sample Size']
-    }
-
-
-def get_report(collection_instrument_type, collection_exercise_id, engine):
-    report_for_type = {
-        'SEFT': get_seft_report,
-        'EQ': get_eq_report
-    }
-    return report_for_type[collection_instrument_type](collection_exercise_id, engine)
-
-
-@response_dashboard_api.route('/<collection_instrument_type>/collection-exercise/<collection_exercise_id>')
+@response_dashboard_api.route('/survey/<survey_id>/collection-exercise/<collection_exercise_id>')
 class ResponseDashboard(Resource):
 
     @staticmethod
-    def get(collection_instrument_type, collection_exercise_id):
+    def get(survey_id, collection_exercise_id):
 
         engine = app.db.engine
 
-        collection_exercise_id = parse_uuid(collection_exercise_id)
-        if not collection_exercise_id:
-            logger.debug('Malformed collection exercise ID', invalid_id=collection_exercise_id)
+        parsed_survey_id = parse_uuid(survey_id)
+        if not parsed_survey_id:
+            logger.debug('Responses dashboard endpoint received malformed survey ID',
+                         invalid_survey_id=survey_id)
+            abort(400, 'Malformed survey ID')
+
+        parsed_collection_exercise_id = parse_uuid(collection_exercise_id)
+        if not parsed_collection_exercise_id:
+            logger.debug('Responses dashboard endpoint received malformed collection exercise ID',
+                         invalid_collection_exercise_id=collection_exercise_id)
             abort(400, 'Malformed collection exercise ID')
 
         try:
-            report = get_report(collection_instrument_type.upper(), collection_exercise_id, engine)
-        except KeyError:
-            abort(404, 'Invalid collection instrument type')
+            report = get_report(parsed_survey_id, parsed_collection_exercise_id, engine)
         except NoDataException:
-            abort(404, 'Invalid collection exercise ID')
+            abort(404, 'Invalid collection exercise or survey ID')
 
         response = {
             'metadata': {
--- test/resources/test_responses_dashboard.py
+++ test/resources/test_responses_dashboard.py
@@ -10,7 +10,7 @@ class TestResponseDashboard(TestCase):
         self.test_client = app.test_client()
 
     @mock.patch('rm_reporting.app.db.engine')
-    def test_dashboard_SEFT_report_success(self, mock_engine):
+    def test_dashboard_report_success(self, mock_engine):
         mock_engine.execute.return_value.first.return_value = {
             'Sample Size': 100,
             'Total Enrolled': 50,
@@ -20,51 +20,8 @@ class TestResponseDashboard(TestCase):
             'Complete': 10
         }
         response = self.test_client.get(
-            '/reporting-api/v1/response-dashboard/SEFT/collection-exercise/33db9feb-bed0-46e8-bcb1-3a0373224cd3')
-        response_dict = json.loads(response.data)
-
-        self.assertEqual(100, response_dict['report']['sampleSize'])
-        self.assertEqual(50, response_dict['report']['accountsEnrolled'])
-        self.assertEqual(30, response_dict['report']['downloads'])
-        self.assertEqual(10, response_dict['report']['uploads'])
-
-    @mock.patch('rm_reporting.app.db.engine')
-    def test_dashboard_SEFT_report_invalid_id(self, mock_engine):
-        mock_engine.execute.return_value.first.return_value = {
-            'Sample Size': 100,
-            'Total Enrolled': 50,
-            'Total Pending': 10,
-            'Not Started': 100,
-            'In Progress': 30,
-            'Complete': None
-        }
-        response = self.test_client.get(
-            '/reporting-api/v1/response-dashboard/SEFT/collection-exercise/00000000-0000-0000-0000-000000000000')
-        error_response = json.loads(response.data)['message']
-
-        self.assertEqual(response.status_code, 404)
-        self.assertEqual(error_response, 'Invalid collection exercise ID')
-
-    def test_dashboard_SEFT_report_malformed_id(self):
-        response = self.test_client.get(
-            '/reporting-api/v1/response-dashboard/SEFT/collection-exercise/not-a-valid-uuid-format')
-        error_response = json.loads(response.data)['message']
-
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(error_response, 'Malformed collection exercise ID')
-
-    @mock.patch('rm_reporting.app.db.engine')
-    def test_dashboard_EQ_report_success(self, mock_engine):
-        mock_engine.execute.return_value.first.return_value = {
-            'Sample Size': 100,
-            'Total Enrolled': 50,
-            'Total Pending': 10,
-            'Not Started': 70,
-            'In Progress': 20,
-            'Complete': 10
-        }
-        response = self.test_client.get(
-            '/reporting-api/v1/response-dashboard/EQ/collection-exercise/33db9feb-bed0-46e8-bcb1-3a0373224cd3')
+            '/reporting-api/v1/response-dashboard/survey/57586798-74e3-49fd-93da-a782ec5f5129'
+            '/collection-exercise/33db9feb-bed0-46e8-bcb1-3a0373224cd3')
         response_dict = json.loads(response.data)
 
         self.assertEqual(200, response.status_code)
@@ -76,7 +33,7 @@ class TestResponseDashboard(TestCase):
         self.assertEqual(70, response_dict['report']['notStarted'])
 
     @mock.patch('rm_reporting.app.db.engine')
-    def test_dashboard_EQ_report_invalid_id(self, mock_engine):
+    def test_dashboard_report_invalid_id(self, mock_engine):
         mock_engine.execute.return_value.first.return_value = {
             'Sample Size': 100,
             'Total Enrolled': 50,
@@ -86,23 +43,27 @@ class TestResponseDashboard(TestCase):
             'Complete': None
         }
         response = self.test_client.get(
-            '/reporting-api/v1/response-dashboard/EQ/collection-exercise/00000000-0000-0000-0000-000000000000')
+            '/reporting-api/v1/response-dashboard/survey/57586798-74e3-49fd-93da-a782ec5f5129'
+            '/collection-exercise/00000000-0000-0000-0000-000000000000')
         error_response = json.loads(response.data)['message']
 
         self.assertEqual(response.status_code, 404)
-        self.assertEqual(error_response, 'Invalid collection exercise ID')
+        self.assertEqual(error_response, 'Invalid collection exercise or survey ID')
 
-    def test_dashboard_EQ_report_malformed_id(self):
+    def test_dashboard_report_malformed_collection_exercise_id(self):
         response = self.test_client.get(
-            '/reporting-api/v1/response-dashboard/EQ/collection-exercise/not-a-valid-uuid-format')
+            '/reporting-api/v1/response-dashboard/survey/57586798-74e3-49fd-93da-a782ec5f5129'
+            '/collection-exercise/not-a-valid-uuid-format')
         error_response = json.loads(response.data)['message']
 
         self.assertEqual(response.status_code, 400)
         self.assertEqual(error_response, 'Malformed collection exercise ID')
 
-    def test_invalid_collection_instrument_type_returns_404(self):
+    def test_dashboard_report_malformed_survey_id(self):
         response = self.test_client.get(
-            '/reporting-api/v1/response-dashboard/NOT_VALID/collection-exercise/00000000-0000-0000-0000-000000000000')
+            '/reporting-api/v1/response-dashboard/survey/not-a-valid-uuid-format'
+            '/collection-exercise/33db9feb-bed0-46e8-bcb1-3a0373224cd3')
+        error_response = json.loads(response.data)['message']
 
-        self.assertEqual(404, response.status_code)
-        self.assertEqual('Invalid collection instrument type', json.loads(response.data)['message'])
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(error_response, 'Malformed survey ID')
