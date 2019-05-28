044a7e6d10d6669069cec4bce4b4a3e51431eac9
Adam Mann <manny160389@hotmail.com>
2019-05-28 13:46:55 +0100

    Fix service name

--- test/test_logger_config.py
+++ test/test_logger_config.py
@@ -20,29 +20,29 @@ class TestLoggerConfig(unittest.TestCase):
     @log_capture()
     def test_success(self, l):
         os.environ['JSON_INDENT_LOGGING'] = '1'
-        logger_initial_config(service_name='ras-backstage')
+        logger_initial_config(service_name='rm-reporting')
         logger = wrap_logger(logging.getLogger())
         logger.error('Test')
         message = l.records[0].msg
 
-        message_contents = '{\n "event": "Test",\n "level": "error",\n "service": "ras-backstage"'
+        message_contents = '{\n "event": "Test",\n "level": "error",\n "service": "rm-reporting"'
         self.assertIn(message_contents, message)
 
     @pytest.mark.filterwarnings(f"ignore:{testfixtures_warning}")
     @log_capture()
     def test_indent_type_error(self, l):
         os.environ['JSON_INDENT_LOGGING'] = 'abc'
-        logger_initial_config(service_name='ras-backstage')
+        logger_initial_config(service_name='rm-reporting')
         logger = wrap_logger(logging.getLogger())
         logger.error('Test')
         message = l.records[0].msg
-        self.assertIn('{"event": "Test", "level": "error", "service": "ras-backstage"', message)
+        self.assertIn('{"event": "Test", "level": "error", "service": "rm-reporting"', message)
 
     @pytest.mark.filterwarnings(f"ignore:{testfixtures_warning}")
     @log_capture()
     def test_indent_value_error(self, l):
-        logger_initial_config(service_name='ras-backstage')
+        logger_initial_config(service_name='rm-reporting')
         logger = wrap_logger(logging.getLogger())
         logger.error('Test')
         message = l.records[0].msg
-        self.assertIn('{"event": "Test", "level": "error", "service": "ras-backstage"', message)
+        self.assertIn('{"event": "Test", "level": "error", "service": "rm-reporting"', message)
