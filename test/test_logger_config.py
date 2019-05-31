40a982dfae8f7d8d466b8870efa91f93d00b2a96
Adam Mann <manny160389@hotmail.com>
2019-05-31 11:05:22 +0100

    Fix security issues (#20)
    * First pass
    
    * Second pass
    
    * Fix service name
    
    * Update API.md
    
    * Add psycopg2 again

--- test/test_logger_config.py
+++ test/test_logger_config.py
@@ -2,38 +2,47 @@ import logging
 import os
 import unittest
 
+import pytest
 from structlog import wrap_logger
 from testfixtures import log_capture
 
 from rm_reporting.logger_config import logger_initial_config
 
+# Supresses the warnings that won't be fixed by the project maintainer until Python 2 is deprecated.
+# More information about this can be found https://github.com/Simplistix/testfixtures/pull/54
+# Remove this and the filterwarnings if this problem ever gets fixed.
+testfixtures_warning = "inspect.getargspec()"
+
 
 class TestLoggerConfig(unittest.TestCase):
 
+    @pytest.mark.filterwarnings(f"ignore:{testfixtures_warning}")
     @log_capture()
     def test_success(self, l):
         os.environ['JSON_INDENT_LOGGING'] = '1'
-        logger_initial_config(service_name='ras-backstage')
+        logger_initial_config(service_name='rm-reporting')
         logger = wrap_logger(logging.getLogger())
         logger.error('Test')
         message = l.records[0].msg
 
-        message_contents = '{\n "event": "Test",\n "level": "error",\n "service": "ras-backstage"'
+        message_contents = '{\n "event": "Test",\n "level": "error",\n "service": "rm-reporting"'
         self.assertIn(message_contents, message)
 
+    @pytest.mark.filterwarnings(f"ignore:{testfixtures_warning}")
     @log_capture()
     def test_indent_type_error(self, l):
         os.environ['JSON_INDENT_LOGGING'] = 'abc'
-        logger_initial_config(service_name='ras-backstage')
+        logger_initial_config(service_name='rm-reporting')
         logger = wrap_logger(logging.getLogger())
         logger.error('Test')
         message = l.records[0].msg
-        self.assertIn('{"event": "Test", "level": "error", "service": "ras-backstage"', message)
+        self.assertIn('{"event": "Test", "level": "error", "service": "rm-reporting"', message)
 
+    @pytest.mark.filterwarnings(f"ignore:{testfixtures_warning}")
     @log_capture()
     def test_indent_value_error(self, l):
-        logger_initial_config(service_name='ras-backstage')
+        logger_initial_config(service_name='rm-reporting')
         logger = wrap_logger(logging.getLogger())
         logger.error('Test')
         message = l.records[0].msg
-        self.assertIn('{"event": "Test", "level": "error", "service": "ras-backstage"', message)
+        self.assertIn('{"event": "Test", "level": "error", "service": "rm-reporting"', message)
