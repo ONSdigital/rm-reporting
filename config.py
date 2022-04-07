8f6c9b47d4eb2252c89b95fa4dd065c1f25ef58a
Amit Sinha <amit@creativeappdevs.com>
2022-04-07 13:59:52 +0100

    Removing spp report from rm-reporting

--- config.py
+++ config.py
@@ -12,13 +12,6 @@ class Config(object):
     BASIC_AUTH = (SECURITY_USER_NAME, SECURITY_USER_PASSWORD)
     DATABASE_URI = os.getenv("DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/ras")
     GOOGLE_CLOUD_PROJECT = os.getenv("GOOGLE_CLOUD_PROJECT")
-    GCS_ENABLED = os.getenv("GCS_ENABLED", "false")
-    GCS_BUCKET_NAME = os.getenv("GCS_BUCKET_NAME")
-    GCS_BUCKET_PREFIX = os.getenv("GCS_BUCKET_PREFIX")
-    S3_BUCKET = os.getenv("S3_BUCKET")
-    S3_ACCESS_KEY = os.getenv("S3_ACCESS_KEY")
-    S3_SECRET_KEY = os.getenv("S3_SECRET_KEY")
-    AWS_ENABLED = os.getenv("AWS_ENABLED", "false")
 
 
 class DevelopmentConfig(Config):
