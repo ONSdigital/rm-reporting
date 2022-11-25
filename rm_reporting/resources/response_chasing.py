e1281225f6ebb1ed283862ec4fa3462ab7be2a02
Adam Mann <manny160389@hotmail.com>
2022-11-25 17:05:46 +0000

    Remove unused code

--- rm_reporting/resources/response_chasing.py
+++ rm_reporting/resources/response_chasing.py
@@ -1,16 +1,12 @@
-import csv
 import io
 import logging
-from datetime import datetime
 
 from flask import make_response
 from flask_restx import Resource
 from openpyxl import Workbook
-from sqlalchemy import text
-from sqlalchemy.exc import SQLAlchemyError
 from structlog import wrap_logger
 
-from rm_reporting import app, response_chasing_api
+from rm_reporting import response_chasing_api
 from rm_reporting.controllers import case_controller, party_controller
 
 logger = wrap_logger(logging.getLogger(__name__))
@@ -112,7 +108,7 @@ class ResponseChasingDownload(Resource):
                     "",
                 ]
                 ws.append(business)
-
+        logger.info("Finished looping over cases")
         wb.active = 1
         wb.save(output)
         wb.close()
@@ -121,70 +117,4 @@ class ResponseChasingDownload(Resource):
         response = make_response(output.getvalue(), 200)
         response.headers["Content-Disposition"] = f"attachment; filename=response_chasing_{collection_exercise_id}.xlsx"
         response.headers["Content-type"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-        return response
-
-
-@response_chasing_api.route("/download-social-mi/<collection_exercise_id>")
-class SocialMIDownload(Resource):
-    @staticmethod
-    def get(collection_exercise_id):
-        output = io.StringIO()
-
-        # Set headers
-        headers = [
-            "Sample Reference",
-            "Status",
-            "Status Event",
-            "Address Line 1",
-            "Address Line 2",
-            "Locality",
-            "Town Name",
-            "Postcode",
-            "Country",
-        ]
-
-        datestr = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-
-        engine = app.db.engine
-
-        case_status = text(
-            "SELECT cg.sampleunitref, cg.status, "
-            "(SELECT cat.shortdescription "
-            "FROM casesvc.caseevent ce "
-            "JOIN casesvc.category cat "
-            "ON ce.categoryfk = cat.categorypk "
-            "WHERE ce.casefk = c.casepk "
-            "AND cat.shortdescription ~ '^[[:digit:]]*$' "
-            "ORDER BY ce.createddatetime DESC LIMIT 1), "
-            "attributes->> 'ADDRESS_LINE1' AS address_line_1, "
-            "attributes->> 'ADDRESS_LINE2' AS address_line_2, "
-            "attributes->> 'LOCALITY' AS locality, "
-            "attributes->> 'TOWN_NAME' AS town_name, "
-            "attributes->> 'POSTCODE' AS postcode, "
-            "attributes->> 'COUNTRY' AS country "
-            "FROM casesvc.case c "
-            "JOIN casesvc.casegroup cg "
-            "ON c.casegroupfk = cg.casegrouppk "
-            "JOIN samplesvc.sampleattributes sa "
-            "ON CONCAT(attributes->> 'TLA', '' , attributes->> 'REFERENCE') = cg.sampleunitref "
-            "WHERE c.sampleunittype = 'H' AND cg.collectionexerciseid = :collection_exercise_id"
-        )
-
-        try:
-            case_details = engine.execute(case_status, collection_exercise_id=collection_exercise_id)
-        except SQLAlchemyError:
-            logger.exception("SQL Alchemy query failed")
-            raise
-
-        my_data = [headers]
-        my_data.extend(case_details)
-
-        writer = csv.writer(output)
-        writer.writerows(my_data)
-
-        response = make_response(output.getvalue(), 200)
-        response.headers["Content-Disposition"] = (
-            f"attachment; filename=social_mi_report_{collection_exercise_id}" f"_{datestr}.csv"
-        )
-        response.headers["Content-type"] = "text/csv"
         return response
