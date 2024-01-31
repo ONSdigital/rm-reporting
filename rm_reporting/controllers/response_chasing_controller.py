5a74e0cbbf14d903ea3240a6fceed097e52f7f60
SteveScorfield <steve.scorfield@ons.gov.uk>
2024-01-31 09:27:00 +0000

    Adding print to check value

--- rm_reporting/controllers/response_chasing_controller.py
+++ rm_reporting/controllers/response_chasing_controller.py
@@ -1,6 +1,7 @@
 import csv
 import io
 import logging
+from datetime import datetime
 from typing import IO, Callable
 from uuid import UUID
 
@@ -67,7 +68,7 @@ def _add_report_data(ce_id: UUID, survey_id: UUID, document_object, append_funct
         business_attributes = business_attributes_map[str(getattr(case, "party_id"))]
         business_name = getattr(business_attributes, "business_name")
         status_change_time = getattr(case, "change_state_timestamp")
-
+        print("Checking time: " + status_change_time)
         respondents_enrolled_for_business = businesses_enrolled_map.get(str(getattr(case, "party_id")), [])
 
         if respondents_enrolled_for_business:
