163d0fb15de2fb09831ad4fa1567e76b90889d5d
SteveScorfield <steve.scorfield@ons.gov.uk>
2024-02-02 10:42:52 +0000

    Relinted

--- rm_reporting/controllers/response_chasing_controller.py
+++ rm_reporting/controllers/response_chasing_controller.py
@@ -66,8 +66,9 @@ def _add_report_data(ce_id: UUID, survey_id: UUID, document_object, append_funct
         sample_unit_ref = getattr(case, "sample_unit_ref")
         business_attributes = business_attributes_map[str(getattr(case, "party_id"))]
         business_name = getattr(business_attributes, "business_name")
-        status_change_timestamp = datetime.strftime(datetime.strptime(getattr(case, "change_state_timestamp"),
-                                                                      "%Y-%m-%d %H:%M:%S.%f %z"), "%Y-%m-%d %H:%M:%S")
+        status_change_timestamp = datetime.strftime(
+            datetime.strptime(getattr(case, "change_state_timestamp"), "%Y-%m-%d %H:%M:%S.%f %z"), "%Y-%m-%d %H:%M:%S"
+        )
 
         respondents_enrolled_for_business = businesses_enrolled_map.get(str(getattr(case, "party_id")), [])
         print("Datetime: " + str(status_change_timestamp))
